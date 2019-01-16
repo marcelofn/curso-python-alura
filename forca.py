bf4c8f6e58e06b6fe8f20a6fc7830364e3dc4b89
mrfranca <marcelo.frneves@gmail.com>
2019-01-16 10:13:18 -0200

    Implementando a logica do jogo

--- forca.py
+++ forca.py
@@ -2,13 +2,25 @@ def jogo():
     print("********************************")
     print("Bem vindo ao jogo de forca!")
     print("********************************")
-    print("Fim")
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
     enforcou = False
     acertou = False
 
     while not acertou and not enforcou:
-        print("jogando...")
+        chute = input("Qual a letra?")
+        chute = chute.strip()
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if letra.upper() == chute.upper():
+                letras_acertadas[index] = letra
+
+            index = index + 1
+            letras_faltando = str(letras_acertadas.count("_"))
+        print(letras_acertadas)
+        print("Ainda faltam acertar {}".format(letras_faltando))
 
 
 if __name__ == "__main__":
