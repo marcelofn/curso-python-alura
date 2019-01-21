fb2d164694d6b37d515032c35ec228406d62dfb7
mrfranca <marcelo.frneves@gmail.com>
2019-01-21 16:41:44 -0200

    Implementando lista de palavras via arquivo

--- forca.py
+++ forca.py
@@ -1,8 +1,22 @@
+import random
+
+
 def jogo():
     print("********************************")
     print("Bem vindo ao jogo de forca!")
     print("********************************")
-    palavra_secreta = "banana".upper()
+
+    arquivo = open('palavras.txt', 'r')
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
     letras_acertadas = ["_" for letra in palavra_secreta]
     enforcou = False
     acertou = False
@@ -26,6 +40,7 @@ def jogo():
         enforcou = erros == 6
         acertou = "_" not in letras_acertadas
         letras_faltando = str(letras_acertadas.count("_"))
+        print(letras_acertadas)
         print("Ainda faltam acertar {} letras.".format(letras_faltando))
     if (acertou):
         print("Você ganhou!!, A palavra é {}".format(palavra_secreta))
